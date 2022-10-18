66662348af0eeb24f70eb213ef23cb6f40fcb228
Collin Laney <cman504430@gmail.com>
2022-10-18 11:21:47 -0400

    normalResponse > requestResponse

--- functions/utils.py
+++ functions/utils.py
@@ -1,10 +1,11 @@
-normalResponses = [
+requestResponses = [
     "Cool, I'm on it sir.",
     "Okay sir, I'm working on it.",
     "Just a second sir."
+    "As you wish, sir."
 ]
 
 morningResponses = [
-    "Good morning sir",
-    "Wake up sir"
+    "Good morning sir.",
+    "Wake up sir."
 ]
