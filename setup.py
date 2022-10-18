4926caed1b7662b15a46b2040ec79ea431fb7e8f
Collin Laney <cman504430@gmail.com>
2022-10-18 11:09:23 -0400

    Add setup installer

---
+++ setup.py
@@ -0,0 +1,12 @@
+import pip
+
+pip.main(["install","pyttsx3"])
+pip.main(["install","speechrecognition"])
+pip.main(["install","pywhatkit"])
+pip.main(["install","webbrowser"])
+pip.main(["install","datetime"])
+pip.main(["install","wikipedia"])
+pip.main(["install","yfinance"])
+pip.main(["install","pyjokes"])
+pip.main(["install","speedtest"])
+pip.main(["install","pyautogui"])
