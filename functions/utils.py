a39dec8be12776e1c992497a65da64ea3fddd1dc
Collin Laney <cman504430@gmail.com>
2022-10-18 11:33:22 -0400

    Quote from - Jake "The Snake" Snyder

--- functions/utils.py
+++ functions/utils.py
@@ -3,6 +3,7 @@ requestResponses = [
     "Okay sir, I'm working on it.",
     "Just a second sir."
     "As you wish, sir."
+    "Ain't nobody got time for that, Except for me sir."
 ]
 
 morningResponses = [
