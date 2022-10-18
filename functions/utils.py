e14dbd2fc1038a370dc1d69d0903072fc411fcb8
Collin Laney <cman504430@gmail.com>
2022-10-18 10:53:18 -0400

    Added CARLSON responses

---
+++ functions/utils.py
@@ -0,0 +1,10 @@
+normalResponses = [
+    "Cool, I'm on it sir.",
+    "Okay sir, I'm working on it.",
+    "Just a second sir."
+]
+
+morningResponses = [
+    "Good morning sir",
+    "Wake up sir"
+]
