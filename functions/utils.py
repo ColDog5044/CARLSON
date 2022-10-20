718ddc48a53130b4083d29352cc22f9b348a583c
Collin Laney <cman504430@gmail.com>
2022-10-20 18:51:30 -0400

    Added MathAnswers

--- functions/utils.py
+++ functions/utils.py
@@ -1,12 +1,16 @@
 requestResponses = [
     "Cool, I'm on it sir.",
     "Okay sir, I'm working on it.",
-    "Just a second sir."
-    "As you wish, sir."
+    "Just a second sir.",
+    "As you wish, sir.",
     "Ain't nobody got time for that, Except for me sir."
 ]
 
 morningResponses = [
     "Good morning sir.",
     "Wake up sir."
+]
+
+mathAnswers = [
+    "9 plus 10 equals 19 and 21"
 ]
